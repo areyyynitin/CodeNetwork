b6fd9b507163ffea7a96dbe2f50a40b649a7226d
niiiit <101655868+niiiit@users.noreply.github.com>
2025-03-11 23:55:45 +0530

    logo removed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,8 @@
 import Link from "next/link";
 import { ModeToggle } from "./ModeToggle";
-import { CodeIcon } from "lucide-react";
+
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import DasboardBtn from "./DasboardBtn";
-import darkImg from "/Projects/netwerkcode/public/DarkImage.png"
-import lightImg from "/Projects/netwerkcode/public/LightImage.png"
-import Image from 'next/image';
-
 
 function Navbar() {
   return (
@@ -17,13 +13,9 @@ function Navbar() {
           href="/"
           className="flex items-center gap-2 font-semibold text-2xl mr-6 font-mono hover:opacity-80 transition-opacity"
         >
-          <Image src={lightImg} alt="LightLogo" className="block dark:hidden" width={50} height={40} />    
-          <Image src={darkImg} alt="DarkLogo" className="hidden dark:block" width={50} height={40} />
-
-
-        {/* <Image src={images1} alt="CodeNetwork Logo" width={60} height={60} /> */}
-          {/* <CodeIcon className="size-8 text-emerald-100" /> */}
-          <span className="bg-gradient-to-r from-black via-gray-500 to-black bg-clip-text text-transparent dark:from-white dark:via-gray-300 dark:to-white">
+          
+ <span className="bg-gradient-to-r from-black via-gray-500 to-black bg-clip-text text-transparent
+           dark:from-white dark:via-gray-300 dark:to-white">
   CodeNetwork
 </span>
 
